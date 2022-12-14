9d5efe95974e90e67dadec806e8c36634011244a
Amremad <a.emad@outlook.com>
2022-12-14 16:45:56 +0200

    changes

--- lib/models/dialog_theme.dart
+++ lib/models/dialog_theme.dart
@@ -13,38 +13,10 @@ class CDialogTheme {
   final TextStyle textStyle;
   final double rowHeight;
 
- final AlphabetsBar alphabetsBar;
-final CurrentLocationTile currentLocationTile;
-final LastPickTile lastPickTile;
-final SearchTile searchTile;
-  //  = AlphabetsBar(
-  //     visible: true,
-  //     backgroundColor: Colors.transparent,
-  //     selectedBackgroundColor: Colors.transparent,
-  //     textStyle: const TextStyle(fontSize: 12),
-  //     selectedTextStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-  //   );
-
-  // // Search Area Area
-  // final bool isShowSearch;
-  // final String searchText;
-  // final String searchHintText;
-
-  // // Current Location Area
-  // final bool isShowCurrentLocation;
-  // final String currentLocationText;
-
-  // //Last Picked Country Area
-  // final bool isShowLastPickCountry;
-  // final String lastPickText;
-  // final Icon lastPickIcon;
-
-  // // alphabet Area
-  // final Color alphabetBackgroundColor;
-  // final Color alphabetSelectedBackgroundColor;
-  // final TextStyle alphabetTextStyle;
-  // final TextStyle alphabetSelectedTextStyle;
-  // final bool isShowAphabetScroll;
+  final AlphabetsBar alphabetsBar;
+  final CurrentLocationTile currentLocationTile;
+  final LastPickTile lastPickTile;
+  final SearchTile searchTile;
 
   // final DefaultTextStyle defaultTextStyle = DefaultTextStyle.of(context);
   const CDialogTheme({
@@ -52,39 +24,16 @@ final SearchTile searchTile;
     this.currentLocationTile = const CurrentLocationTile(),
     this.lastPickTile = const LastPickTile(),
     this.searchTile = const SearchTile(),
-
     this.appBar,
     this.isShowFlage = true,
     this.isShowFloatButton = true,
-    // this.isShowAphabetScroll = true,
     this.backgroundColor = Colors.transparent,
-    // this.currentLocationText = "Current Location",
     this.titlesStyle = const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
     this.titlesBackground = const Color(0xfff4f4f4),
     this.isShowDialCode = true,
-    // this.isShowSearch = true,
-    // this.searchText = "Search",
-    // this.searchHintText = "name/dial code...",
-    // this.isShowCurrentLocation = true,
-    // this.isShowLastPickCountry = true,
-    // this.lastPickText = "Last Pick",
-    // this.alphabetBackgroundColor = Colors.transparent,
-    // this.alphabetSelectedBackgroundColor = Colors.transparent,
-    // this.alphabetTextStyle = const TextStyle(fontSize: 12),
-    // this.alphabetSelectedTextStyle = const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-    // this.lastPickIcon = const Icon(Icons.check),
     this.textStyle = const TextStyle(fontSize: 16),
     this.rowHeight = 50.0,
-  }) ;
-  // {
-  //   alphabetsBar  = AlphabetsBar(
-  //     visible: true,
-  //     backgroundColor: Colors.transparent,
-  //     selectedBackgroundColor: Colors.transparent,
-  //     textStyle: const TextStyle(fontSize: 12),
-  //     selectedTextStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-  //   );
-  // }
+  });
 }
 
 class AlphabetsBar {
@@ -106,7 +55,7 @@ class AlphabetsBar {
 class CurrentLocationTile {
   final bool visible;
   final String title;
- const CurrentLocationTile({
+  const CurrentLocationTile({
     this.visible = true,
     this.title = "Current Location",
   });
@@ -116,7 +65,7 @@ class LastPickTile {
   final bool visible;
   final String title;
   final Icon icon;
- const LastPickTile({
+  const LastPickTile({
     this.visible = true,
     this.title = "Last Pick",
     this.icon = const Icon(Icons.check),
@@ -127,7 +76,7 @@ class SearchTile {
   final bool visible;
   final String title;
   final String hint;
- const  SearchTile({
+  const SearchTile({
     this.visible = true,
     this.title = "Search",
     this.hint = "name/dial code...",
